e4f6049d02a201996073f02d4f0db1f86cf146f9
babajide234 <babajide234@gmail.com>
2023-10-14 10:42:12 +0100

    fix:hero button

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 import PropTypes from 'prop-types';
 
-export const ButtonLink = ({ children, url }) => {
+export const ButtonLink = ({ children, url,...rest }) => {
   return (
     <Link
         to={url}
-        className=""
+        className=" mt-5 text-neutral border-secondary border-2 px-10 font-bold hover:scale-105 hover:bg-secondary transition-all ease-in-out py-3 rounded-full "
+        {...rest}
     >{children}</Link>
   )
 }
